4a5149b98aa660d23bac5b764c02b9001b75d785
at0m741 <louistouz@icloud.com>
2024-10-18 13:06:02 +0200

    Update avx512_MPI.c

--- avx512_MPI.c
+++ avx512_MPI.c
@@ -5,7 +5,7 @@
 #include <omp.h>  
 #include <memkind.h>   
 
-#define N 8192    
+#define N 4096
 #define BLOCK_SIZE 256  
 
 void mat_mult_blocked(double *A_local, double *B_transposed, double *C_local, int n_local, int N_global) {
